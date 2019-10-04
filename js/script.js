cdc32b93f5b556dbcf23d0fafc79cc1c2833bd46
Dariusz Okonski <darek200180@gmail.com>
2019-10-04 14:34:10 +0100

    add dom object to contain all needed DOM elements from index.html in script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const dom = {
+  errorDom: document.querySelector('.error-js'),
+  inputLoanAmount: document.querySelector('.input-loan-amount-js'),
+  inputInterest: document.querySelector('.input-interest-js'),
+  inputYearsToPay: document.querySelector('.input-years-to-pay-js'),
+
+  btnCalculate: document.querySelector('.btn-calculate-js'),
+  btnClear: document.querySelector('.btn-clear-js'),
+
+  loadingDom: document.querySelector('.loading-js'),
+  resultsDom: document.querySelector('.results-js'),
+
+  outputMonthlyPayment: document.querySelector('.output-monthly-payment-js'),
+  outputTotalPayment: document.querySelector('.output-total-payment-js'),
+  outputTotalInterest: document.querySelector('.output-total-intrest-js'),
+}
